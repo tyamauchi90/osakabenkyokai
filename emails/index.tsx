3250a0359802a4950dcdf66d2bece227d0489448
tyamauchi90 <pecopon24@gmail.com>
2023-11-29 18:19:16 +0900

    Exclude react-email from tracking

---
+++ emails/index.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@react-email/button";
+import { Html } from "@react-email/html";
+
+export default function Email() {
+  return (
+    <Html>
+      <Button
+        href="https://example.com"
+        style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
+      >
+        Click me
+      </Button>
+    </Html>
+  );
+}
