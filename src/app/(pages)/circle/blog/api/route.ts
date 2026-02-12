b4c3b2f9d543eff2740f33eebcd8b7fc94b119c7
tyamauchi90 <pecopon24@gmail.com>
2026-02-12 16:51:55 +0900

    FacebookAPI修正・undiciバージョン調整

--- src/app/(pages)/circle/blog/api/route.ts
+++ src/app/(pages)/circle/blog/api/route.ts
@@ -4,53 +4,57 @@ import { FacebookPostType } from "../../../../type/facebookPostType";
 
 // GET
 export async function GET(
-	req: NextRequest
+  req: NextRequest
 ): Promise<void | NextResponse<FacebookPostType[] | undefined>> {
-	try {
-		const accessToken = process.env.NEXT_PUBLIC_FACEBOOK_ACCESS_TOKEN;
-		const pageId = process.env.NEXT_PUBLIC_FACEBOOK_PAGE_ID;
-		const now = new Date();
-		const twoYearsAgo = new Date(now.getFullYear() - 2, now.getMonth(), now.getDate());
-		const apiEndpoint = `https://graph.facebook.com/v19.0/${pageId}/feed?fields=message,created_time,full_picture,attachments{media,subattachments,image,src}&since=${Math.floor(
-			twoYearsAgo.getTime() / 1000
-		)}`;
-		// const apiEndpoint = `https://graph.facebook.com/v19.0/${pageId}/feed?fields=message,created_time,full_picture,attachments{media,subattachments,image,src}`;
-		let allPosts: FacebookPostType[] = [];
-		let next = apiEndpoint;
+  try {
+    const accessToken = process.env.NEXT_PUBLIC_FACEBOOK_ACCESS_TOKEN;
+    const pageId = process.env.NEXT_PUBLIC_FACEBOOK_PAGE_ID;
+    const now = new Date();
+    const twoYearsAgo = new Date(
+      now.getFullYear() - 2,
+      now.getMonth(),
+      now.getDate()
+    );
+    const apiEndpoint = `https://graph.facebook.com/v21.0/${pageId}/feed?fields=message,created_time,full_picture,attachments{media,subattachments,image,src}&since=${Math.floor(
+      twoYearsAgo.getTime() / 1000
+    )}`;
+    // const apiEndpoint = `https://graph.facebook.com/v19.0/${pageId}/feed?fields=message,created_time,full_picture,attachments{media,subattachments,image,src}`;
+    let allPosts: FacebookPostType[] = [];
+    let next = apiEndpoint;
 
-		while (next) {
-			const response = await axios.get(next, {
-				headers: {
-					Authorization: `Bearer ${accessToken}`,
-				},
-			});
-			const { data, paging } = response.data as {
-				data: FacebookPostType[];
-				paging: { next: string };
-			};
-			allPosts = allPosts.concat(data);
-			next = paging && paging.next;
+    while (next) {
+      const response = await axios.get(next, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      const { data, paging } = response.data as {
+        data: FacebookPostType[];
+        paging: { next: string };
+      };
+      allPosts = allPosts.concat(data);
+      next = paging && paging.next;
 
-			if (!next) {
-				break;
-			}
-		}
-		return NextResponse.json(allPosts);
-	} catch (error: unknown) {
-		if (axios.isAxiosError(error)) {
-			const axiosError = error as AxiosError;
-			if (axiosError.response) {
-				console.error("Response Error: ", axiosError.response.data);
-			} else if (axiosError.request) {
-				console.error("Request Error: ", axiosError.request);
-			} else {
-				console.error("Setting Error: ", axiosError.message);
-			}
-			console.error("Axios Config: ", axiosError.config);
-		} else {
-			console.error("Unknown Error: ", error);
-		}
-		// return [];
-		return new NextResponse(null, { status: 500 });
-	}
+      if (!next) {
+        break;
+      }
+    }
+    return NextResponse.json(allPosts);
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      const axiosError = error as AxiosError;
+      if (axiosError.response) {
+        console.error("Response Error: ", axiosError.response.data);
+      } else if (axiosError.request) {
+        console.error("Request Error: ", axiosError.request);
+      } else {
+        console.error("Setting Error: ", axiosError.message);
+      }
+      console.error("Axios Config: ", axiosError.config);
+    } else {
+      console.error("Unknown Error: ", error);
+    }
+    // return [];
+    return new NextResponse(null, { status: 500 });
+  }
 }
